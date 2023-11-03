a3552b43c71538a5f7db287b824e0bf683300c10
Caio "Quintas_" Santiago <caioquintassantiago@gmail.com>
2023-11-02 21:13:37 -0300

    Revert "add temaplate string support"
    This reverts commit 49878b350725f9b1660dcbb03dd3fb0963db7cf1.

--- src/core/strategy.ts
+++ src/core/strategy.ts
@@ -1,8 +1,5 @@
 import {Result} from "./result";
-import {Primitive} from "./utils/types/primitive";
 
 export interface Strategy {
-    rawQuery<ReturnValueType = any>(query: string, values: Primitive[]): Promise<Result<ReturnValueType>>
-
-    ping()
+    rawQuery<ReturnValueType = any>(query: string): Promise<Result<ReturnValueType>>
 }
--- src/core/utils/templateStrings/sql.ts
+++
@@ -1,9 +0,0 @@
-import {Primitive} from "../types/primitive";
-
-
-export function sql(strings: TemplateStringsArray, ...values: Primitive[]): [string, Primitive[]] {
-    let result = strings[0] ?? '';
-    for (let i = 1; i < strings.length; i++)
-        result += `$${i}${strings[i] ?? ''}`;
-    return [result, values]
-}
