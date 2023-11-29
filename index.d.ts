fcd5f0dbb43f5dddfd261cf805708d2083cf6c42
Caio "Quintas_" Santiago <caioquintassantiago@gmail.com>
2023-11-29 10:41:48 -0300

    test

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-import "./core/utils/templateStrings/sql";
-import "./core/utils/templateStrings/partiql";
-import "./core/result";
-import "./spectre";
-//# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
+/// <reference types="node" />
+
 import "./core/utils/templateStrings/sql"
 import "./core/utils/templateStrings/partiql"
 
 import "./core/result"
 
 import "./spectre"
+
+import * as spectre from "."
+
+
+export const native: typeof spectre | null = null;
+
