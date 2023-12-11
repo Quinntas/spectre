b877b7f5869d2cf4fa9a208111438f53b47d75db
Quinntas <caioquintassantiago@gmail.com>
2023-12-11 16:38:04 -0300

    fix

--- src/dynamo/dynamo.ts
+++ src/dynamo/dynamo.ts
@@ -2,6 +2,7 @@ import {DynamoDBClient, ExecuteTransactionCommand} from "@aws-sdk/client-dynamod
 import {QueryDTO, Strategy} from "../core/strategy";
 import {result, Result, SpectreError} from "../core/result";
 import {DynamoDBDocumentClient, ExecuteStatementCommand,} from "@aws-sdk/lib-dynamodb";
+import {AttributeValue, ParameterizedStatement} from "@aws-sdk/client-dynamodb/dist-types/models/models_0";
 import {Primitive} from "../core/utils/types/primitive";
 
 interface AWSConfig {
@@ -11,6 +12,12 @@ interface AWSConfig {
         secretAccessKey: string
     }
 }
+
+type ParameterType = 'S' | 'N'
+
+type Parameter = {
+    [key in ParameterType]: Primitive;
+};
 
 export class Dynamo implements Strategy {
     private readonly client: DynamoDBClient
@@ -40,6 +47,9 @@ export class Dynamo implements Strategy {
 
     private errorHandler(err: Error): Result<any> {
         switch (err.name) {
+            case "DuplicateItemException":
+                return result(false, err.message, true, SpectreError.DATABASE_DUPLICATE_ENTRY);
+            case "TransactionCanceledException":
             case "ResourceNotFoundException":
                 return result(false, err.message, true, SpectreError.DATABASE_BAD_REQUEST);
             default:
@@ -47,16 +57,31 @@ export class Dynamo implements Strategy {
         }
     }
 
+    private parameterizeValues(values: Primitive[]) {
+        let result: Parameter[] = []
+        values.forEach(value => {
+            if (typeof value === 'string')
+                result.push({S: value} as Parameter)
+            else if (typeof value === 'number')
+                result.push({N: value.toString()} as Parameter)
+            else
+                throw new Error(`Type ${typeof value} not supported`)
+        })
+        return result
+    }
+
     public async rawQuery<ReturnValueType = any | any[]>(queryDTO: QueryDTO): Promise<Result<ReturnValueType>> {
-        let command: ExecuteStatementCommand;
+        let command;
 
         if (Array.isArray(queryDTO)) {
-            const dto: { Statement: string, Parameters: Primitive[] }[] = queryDTO.map(query => ({
+            const dto = queryDTO.map(query => ({
                 Statement: query.query,
-                Parameters: query.values,
-            }))
+                Parameters: this.parameterizeValues(query.values) as AttributeValue[],
+            } as ParameterizedStatement))
 
-            command = new ExecuteTransactionCommand(dto);
+            command = new ExecuteTransactionCommand({
+                TransactStatements: dto
+            });
         } else
             command = new ExecuteStatementCommand({
                 Statement: queryDTO.query,
@@ -65,7 +90,7 @@ export class Dynamo implements Strategy {
             });
 
         try {
-            const resultValues = await this.docClient.send(command)
+            const resultValues = await this.docClient.send(command) as any
             return result<ReturnValueType>(resultValues.Items.length > 0, resultValues as ReturnValueType, false);
         } catch (e) {
             throw this.errorHandler(e)
